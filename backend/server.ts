db0ce74b90931a6dd159c617ef9858fc9f03225c
Odair Michael Bendotti <obendotti@gmail.com>
2026-05-12 15:05:15 -0300

    feat: change to deploy

---
+++ backend/server.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import { UserRoutes } from "./src/http/routes/UserRoutes.js";
+import { CompanyRoutes } from "./src/http/routes/CompanyRoutes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { EmpenhoRoutes } from "./src/http/routes/EmpenhoRoutes.js";
+import { NotaFiscalRoutes } from "./src/http/routes/NotaFiscalRoutes.js";
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: "*",
+  }),
+);
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api", UserRoutes);
+app.use("/api", CompanyRoutes);
+app.use("/api", EmpenhoRoutes);
+app.use("/api", NotaFiscalRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`http://74.50.67.174:${process.env.PORT}`);
+});
